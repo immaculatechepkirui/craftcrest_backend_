7e183c829c6858f3ed35ae2cba6e9c66bdfd431f
Immaculate <chepkiruiimmaculatekogo@gmail.com>
2025-10-28 09:57:41 +0300

    requirements

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="craftcrest_backend",
+    version="0.0.0",
+    description="Temporary package metadata for building wheels",
+    packages=find_packages(),
+)
